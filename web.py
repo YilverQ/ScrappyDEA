93c2cc17966ca58b8d998c882f3463f10a38eee2
YilverQ <yilver0906@gmail.com>
2024-07-24 12:48:35 -0400

    Working July

--- web.py
+++ web.py
@@ -5,69 +5,68 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
+from selenium.common.exceptions import NoSuchElementException
 import time
 from datetime import datetime
+import json
 
 
 #Ejecutar el navegador. 
 driver = webdriver.Firefox()
-#Ingresar a la ruta URL. 
-driver.get("http://192.168.0.1/") #Aquí irá la URL a la que quieras ir.
-#Eseperar que la página recargue. 
-time.sleep(2)
 
 
-#Datos para ingresar en el login.
-name = "yilver"
-password = "Yil09Que06#"
+"""Estados"""
+def getEstados():
+	#Ingresar a la ruta URL. 
+	driver.get("https://codigosplanteles.info/")
+	time.sleep(2)
+
+	cajaEstados = driver.find_element("xpath" ,'/html/body/div[1]/div[1]/div/div/main/article/div/div[3]/div/div/div[3]')
+	estados = cajaEstados.find_elements(By.CLASS_NAME, "uagb-tax-link")
+
+	# Iterar sobre cada estado
+	lista_enlaces_estados = []
+	for estado in estados:
+		enlace = estado.get_attribute("href")
+		titulo = estado.find_element(By.CLASS_NAME, "uagb-tax-title").text
+		lista_enlaces_estados.append({"Estado": titulo, "Enlace": enlace})
+
+	return lista_enlaces_estados
 
 
-"""Login"""
-usuario = driver.find_element("xpath" ,'//*[@id="userName"]')
-clave = driver.find_element("xpath" ,'//*[@id="pcPassword"]') #Selecciona elementos xpath.
-usuario.send_keys(name) #teclea el texto.
-clave.send_keys(password)
-#Hacer click en el login.
-boton = driver.find_element("xpath", '//*[@id="loginBtn"]').click()
+
+def getEscuelas(link_estado):
+    driver.get(link_estado)
+    lista_enlaces_escuelas = []
+
+    while True:
+        caja_escuelas = driver.find_element("xpath", '//*[@id="page"]')
+        links = caja_escuelas.find_elements(By.TAG_NAME, 'a')
+
+        try:
+            load_more_button = driver.find_element("xpath", '//*[@class="ast-load-more no-more active"]')
+            if load_more_button:
+                break
+        except NoSuchElementException:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)
+
+    for link in links:
+        if link.get_attribute("rel") == "bookmark":
+            enlace = link.get_attribute("href")
+            lista_enlaces_escuelas.append(enlace)
+
+    return lista_enlaces_escuelas
 
 
 
-"""Dentro del router"""
-"""Primer frame"""
-"""Control de Acceso y Regla"""
-time.sleep(1) #Es necesario esperar 1 segundo.
-frame = driver.find_element("xpath", '//*[@id="frame1"]')
-driver.switch_to.frame(frame)
-control_acceso = driver.find_element("xpath", '//*[@id="menu_fw"]').click()
-reglar = driver.find_element("xpath", '//*[@id="menu_fwrule"]').click()
+lista_enlaces_estados = getEstados()
+lista_enlaces_escuelas = []
+for estado in lista_enlaces_estados: 
+	lista_escuelas = getEscuelas(estado["Enlace"])
+	lista_enlaces_escuelas.append({estado["Estado"] : lista_escuelas})
 
-"""Segundo frame"""
-"""Bloque de usuario"""
-driver.switch_to.default_content()
-frame = driver.find_element("xpath", '//*[@id="frame2"]')
-driver.switch_to.frame(frame)
-#Seleccionamos el dispositivo.
-checkbox = driver.find_element("xpath", '//*[@id="fwid6"]').click()
-bloquear = driver.find_element("xpath", '/html/body/div[1]/div/div/div/p[7]/input[2]').click()
+	with open("Escuelas.json", "w") as file:
+		json.dump(lista_enlaces_escuelas, file, indent=4)
 
-"""tercer frame"""
-"""Finalizar sesión"""
-driver.switch_to.default_content()
-frame = driver.find_element("xpath", '//*[@id="frame1"]')
-driver.switch_to.frame(frame)
-finalizar_sesion = driver.find_element("xpath", '//*[@id="menu_logout"]').click()
-
-
-"""Cerrar Alerta JavaScript"""
-time.sleep(1)
-alert = Alert(driver) 
-alert.accept() 
-
-
-"""Cerrar el navegador."""
-time.sleep(2)
-driver.close()
-
-"""
-	Mayeuri: //*[@id="fwid4"]
-"""
+	print(estado["Estado"], len(lista_escuelas))
